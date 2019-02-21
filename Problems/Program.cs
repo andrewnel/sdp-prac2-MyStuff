ec0ff78be0dc806014e3780f5a445609b729f671
Andrew Nel <andrew.nel.an@gmail.com>
2019-02-21 15:55:57 +0200

    Including Prefix(string) method in Program.cs

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
+        public static string Prefix(string input)
+        {
+            if(input == null)
+                return null;
+            else
+            {
+                string[] split = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                return($"{input.Length}, {split.Length}:{input}");
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(Prefix("hello"));
+            Console.WriteLine(Prefix(""));
+            Console.WriteLine(Prefix("what    ...  did you say?? "));
         }
     }
 }
